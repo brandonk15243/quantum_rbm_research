5c1957026be06d8eaf937b784ec0bd3b4df95c1e
Brandon Kang <brandonk15243@gmail.com>
2023-03-20 04:42:04 -0700

    Added support for obc (not tested)

--- src/quantum_rbm_research/Hamiltonian.py
+++ src/quantum_rbm_research/Hamiltonian.py
@@ -10,17 +10,19 @@ Transverse Ising Hamiltonian with open boundary conditions
 
 
 class TransverseIsingHamiltonian():
-    def __init__(self, N, J, h):
+    def __init__(self, N, J, h, obc=False):
         # Params
         self.N = N
         self. J = J
         self.h = h
+        self.obc = obc
 
         # Create matrix
         self.H = torch.zeros(2 ** self.N, 2 ** self.N)
+        N = (self.N - 1) if obc else self.N
         for i in range(0, self.N):
             # Interaction term
-            j = (i + 1) % N
+            j = (i + 1) % self.N
             spinz_i = utils.operator_at(utils.sigma_z(), i, self.N)
             spinz_j = utils.operator_at(utils.sigma_z(), j, self.N)
             self.H += -self.J * spinz_i @ spinz_j
